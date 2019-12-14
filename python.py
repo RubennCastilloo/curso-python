a29ba70b6491d11b534f9f91e097f8005d6e7f26
Ruben Castillo <rcastillo@raivtech.com>
2019-12-14 14:30:51 -0700

    Crear cuadro con Turtle
    Crear cuadro con Turtle

---
+++ python.py
@@ -0,0 +1,14 @@
+import turtle
+
+window = turtle.Screen()
+ruben = turtle.Turtle()
+ruben.forward(50)
+ruben.left(90)
+ruben.forward(50)
+ruben.left(90)
+ruben.forward(50)
+ruben.left(90)
+ruben.forward(50)
+ruben.left(90)
+
+turtle.mainloop()
